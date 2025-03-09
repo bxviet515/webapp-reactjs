6b15ae140d3c235ceb8052068eebe3ddfdc8f98c
bxviet515 <vietsdk6@gmail.com>
2025-03-09 21:57:08 +0700

    init project and base entity

---
+++ src/main/java/com/xuanviet/webapp_reactjs/WebappReactjsApplication.java
@@ -0,0 +1,13 @@
+package com.xuanviet.webapp_reactjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappReactjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappReactjsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xuanviet/webapp_reactjs/WebappReactjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuanviet.webapp_reactjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappReactjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
